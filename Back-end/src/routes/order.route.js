3bd356cb78884cad0045eb6b49a28a3e989244ca
hhuydev <hahuy.dev@gmail.com>
2021-12-19 16:48:11 +0700

    finished order route

--- Back-end/src/routes/order.route.js
+++ Back-end/src/routes/order.route.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const { addOrderItems } = require("../controller/OrderController");
+const {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+  updateOrderToDeliver,
+} = require("../controller/OrderController");
+const { auth, adminAuth } = require("../middelwares/auth");
 
 const router = express.Router();
 
-router.post("/create-order", addOrderItems);
+router.route("/").post(auth, addOrderItems).get(auth, adminAuth, getOrders);
+router.route("/myorder").get(auth, getMyOrders);
+router.route("/:id").get(auth, getOrderById);
+router.route("/:id/pay").put(auth, updateOrderToPaid);
+router.route("/:id/deliver").put(auth, adminAuth, updateOrderToDeliver);
 
 module.exports = router;
